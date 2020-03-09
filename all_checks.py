33e86de74d43c0b22dc8587542ee8827d035c0a4
seanw7 <seanw@protonmail.ch>
2020-03-09 14:53:42 -0700

    Added network check function

--- all_checks.py
+++ all_checks.py
@@ -25,11 +25,20 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise"""
     return check_disk_full(disk='/', min_gb=2, min_percent=10)
 
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
 
 def main():
     checks = [
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
+        (check_no_network, "No working network."),
     ]
     everything_ok = True
     for check, msg in checks:
