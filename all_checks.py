412f5627aae6165240a26aa5f994e8cc6e890428
seanw7 <seanw@protonmail.ch>
2020-03-09 14:52:44 -0700

    Initial commit of all_checks.py scripy

---
+++ all_checks.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import os
+import sys
+import shutil
+import socket
+import psutil
+
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise"""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise"""
+    return check_disk_full(disk='/', min_gb=2, min_percent=10)
+
+
+def main():
+    checks = [
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full"),
+    ]
+    everything_ok = True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok = False
+            # sys.exit(1)
+
+    if not everything_ok:
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
+
+
+main()
